2082c573c0bed47973adcc8d0e31f4b74fe47cf3
Jeremy Thaller <jeremy@thaller.dev>
2024-06-23 15:57:19 -0400

    Update get_pdf_url.py

--- get_pdf_url.py
+++ get_pdf_url.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 def get_pdf_url(url):
     # Send a GET request to the specified URL
@@ -15,17 +16,18 @@ def get_pdf_url(url):
         if div_tag:
             # Extract the value of the data-file attribute
             pdf_url = div_tag.get('data-file')
-            return pdf_url
+            if pdf_url:
+                print(pdf_url)
+                return
+            else:
+                print("Data-file attribute not found.", file=sys.stderr)
         else:
-            print("Div tag with class 'pdf-container loaded' not found.")
+            print("Div tag with class 'pdf-container loaded' not found.", file=sys.stderr)
     else:
-        print(f"Failed to fetch the URL. Status code: {response.status_code}")
+        print(f"Failed to fetch the URL. Status code: {response.status_code}", file=sys.stderr)
+
+    sys.exit(1)
 
 # Example usage
 url = "https://github.com/jthaller/resume_url_hosting/blob/main/Jeremy_Thaller_Resume.pdf"
-pdf_url = get_pdf_url(url)
-
-if pdf_url:
-    print(f"The PDF URL is: {pdf_url}")
-else:
-    print("Failed to retrieve the PDF URL.")
+get_pdf_url(url)
