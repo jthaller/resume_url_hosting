5da6021cffbfc8977d0e5202fddf55c5b6593580
Jeremy Thaller <jeremy@thaller.dev>
2024-06-23 16:18:11 -0400

    Update get_pdf_url.py

--- get_pdf_url.py
+++ get_pdf_url.py
@@ -1,33 +1,27 @@
 import requests
-from bs4 import BeautifulSoup
 import sys
 
-def get_pdf_url(url):
-    # Send a GET request to the specified URL
+def get_latest_commit_sha(repo, branch='main'):
+    url = f"https://api.github.com/repos/{repo}/commits/{branch}"
     response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
+    
     if response.status_code == 200:
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Find the div tag with the class 'pdf-container loaded'
-        div_tag = soup.find('div', class_='pdf-container loaded')
-        if div_tag:
-            # Extract the value of the data-file attribute
-            pdf_url = div_tag.get('data-file')
-            if pdf_url:
-                print(pdf_url)
-                return
-            else:
-                print("Data-file attribute not found.", file=sys.stderr)
+        commit_sha = response.json().get('sha')
+        if commit_sha:
+            return commit_sha
         else:
-            print("Div tag with class 'pdf-container loaded' not found.", file=sys.stderr)
+            print("Commit SHA not found.", file=sys.stderr)
     else:
-        print(f"Failed to fetch the URL. Status code: {response.status_code}", file=sys.stderr)
+        print(f"Failed to fetch the latest commit. Status code: {response.status_code}", file=sys.stderr)
 
     sys.exit(1)
 
-# Example usage
-url = "https://github.com/jthaller/resume_url_hosting/blob/main/Jeremy_Thaller_Resume.pdf"
-get_pdf_url(url)
+def get_pdf_url(latest_commit_hash: str) -> str:
+    return f"https://raw.githubusercontent.com/jthaller/resume_url_hosting/{latest_commit_hash}/Jeremy_Thaller_Resume.pdf"
+
+
+if __name__ == "__main__":
+    repo = "jthaller/resume_url_hosting"
+    latest_commit_hash = get_latest_commit_sha(repo)
+    pdf_url = get_pdf_url(latest_commit_hash)
+    print(pdf_url)  # Print the PDF URL to capture it in the GitHub Actions workflow
